dc7c1ed090075e8a8a7ca1a441ea8cd5309f0be5
Michael Nguyen <ngxphong03@gmail.com>
2025-02-28 22:16:25 +0700

    update

--- src/pages/Home/Introduction.jsx
+++ src/pages/Home/Introduction.jsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
 import IntroductionBanner from "@/assets/images/intro-banner.svg";
+import { motion } from "framer-motion";
 
 export default function Introduction() {
   const containerVariants = {
